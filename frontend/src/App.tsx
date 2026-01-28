056aa2c7b08a7666f3ba7ac25177f805ace2ae0d
Hizkia Halim <hizkiahalim2412001@gmail.com>
2026-01-28 22:12:57 +0700

    Logo and char desc navigation
    Logo and character desc navigation. Made using random and hardcoded data. Already has dark / light mode in some places and mobile friendly

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,110 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import lunaLogo from "/luna-logo.png";
+import mageIcon from "/mage.png";
+import warriorIcon from "/swords.png";
+import archerIcon from "/archery.png";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div className='main-div'>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+    <div className="main-div">
+      <div className="logo-div">
+        <a
+          href="https://www.google.com/search?client=opera-gx&q=luna&sourceid=opera&ie=UTF-8&oe=UTF-8"
+          target="_blank"
+        >
+          <img src={lunaLogo} className="logo" alt="Luna logo" />
+          <p className="logo-p">Luna Skill Calculator</p>
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <div className="main-card">
+        <div className="char-desc-div">
+          <div className="race-div">
+            <p className="race-p"> Select Race </p>
+            <div className="button-groups">
+              <button
+                onClick={() => setCount((count) => count + 1)}
+                title="mobil buat 1 keluarga besar"
+              >
+                Elf
+              </button>
+              <button
+                onClick={() => setCount((count) => count + 1)}
+                title="kepiting, mau apa lagi?"
+              >
+                Crab
+              </button>
+            </div>
+          </div>
+          <div className="class-div">
+            <p className="class-p"> Select Class </p>
+            <div className="button-groups">
+              <button
+                onClick={() => setCount((count) => count + 1)}
+                title="mage kayak kontol"
+              >
+                <img src={mageIcon} className="class-icon" />
+              </button>
+              <button
+                onClick={() => setCount((count) => count + 1)}
+                title="warrior chad"
+              >
+                <img src={warriorIcon} className="class-icon" />
+              </button>
+              <button
+                onClick={() => setCount((count) => count + 1)}
+                title="archer pew pew"
+              >
+                <img src={archerIcon} className="class-icon" />
+              </button>
+            </div>
+          </div>
+          <div className="level-div">
+            <p className="level-p"> Level 20 </p>
+            <div className="select">
+              <select>
+                <option>Guardian</option>
+                <option>Pecundang</option>
+              </select>
+            </div>
+            <p className="level-p"> Level 40 </p>
+            <div className="select">
+              <select>
+                <option>Gabut</option>
+                <option>Gabut</option>
+                <option>Gabut</option>
+                <option>Gabut</option>
+              </select>
+            </div>
+            <p className="level-p"> Level 75 </p>
+            <div className="select">
+              <select>
+                <option>Whaler</option>
+                <option>Whaler</option>
+                <option>Whaler</option>
+                <option>Whaler</option>
+              </select>
+            </div>
+            <p className="level-p"> Level 105 </p>
+            <div className="select">
+              <select>
+                <option>Master</option>
+                <option>Master Baiter</option>
+                <option>Master</option>
+                <option>Sword Master</option>
+              </select>
+            </div>
+          </div>
+        </div>
+        <div className="skill-selection-div"></div>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;

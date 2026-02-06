36c675d34edacad84e75219b2a2e3d6b01364a97
Hizkia Halim <hizkiahalim2412001@gmail.com>
2026-02-06 10:52:54 +0700

    change query job skill
    update job skill list query to also get starting skills

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,10 @@ import fs from 'node:fs/promises';
 
 import express from 'express';
 import pool from './db.js';
+<<<<<<< Updated upstream
+=======
+import { json } from 'node:stream/consumers';
+>>>>>>> Stashed changes
 
 const app = express();
 
@@ -74,6 +78,76 @@ app.post('/get-job-list', async(req, res) => {
   res.status(200).json({text: 'job-list', query: jobs.rows})
 });
 
+<<<<<<< Updated upstream
+=======
+app.post('/get-skill-list', async(req, res) => {
+  const {charDetail} = req.body;
+
+  const baseJobId = await pool.query("\
+      SELECT\
+        jl.job_id\
+      FROM\
+        job_list jl\
+      WHERE\
+        jl.race != 'Devil'\
+        AND jl.level != 145 \
+        AND jl.level = 0\
+        AND jl.race = '"+charDetail.race+"'\
+        AND jl.job_name = '"+charDetail.class+"'\
+    ");
+
+
+  const jobIds = Object.entries(charDetail)
+    .filter(([key, value]) => key.startsWith('jobId') && value !== 'None')
+    .map(([, value]) => value);
+
+  jobIds.push(baseJobId.rows[0]?.job_id);
+
+  const skills = await pool.query(
+    `
+        SELECT\
+            jl.job_name,\
+            jl.race,\
+            js.skill_id,\
+            regexp_replace(s.skill_name, '\^s', ' ', 'g') AS skill_name,\
+            st.skill_tooltip,\
+            regexp_replace(sb1.skill_name, '\^s', ' ', 'g') AS buff1,\
+            regexp_replace(sb2.skill_name, '\^s', ' ', 'g') AS buff2,\
+            regexp_replace(sb3.skill_name, '\^s', ' ', 'g') AS buff3,\
+          regexp_replace(sb4.skill_name, '\^s', ' ', 'g') AS buff4,\
+            regexp_replace(sb5.skill_name, '\^s', ' ', 'g') AS buff5\
+        FROM job_list jl\
+          JOIN job_skill js\
+              ON jl.job_id = js.job_id\
+          JOIN skills s\
+              ON s.skill_idx = js.skill_id\
+          JOIN skill_tooltip st\
+              ON st.skill_idx = s.skill_idx\
+          LEFT JOIN skills_buff sb1\
+              ON s.buff_id_1 = sb1.skill_idx\
+          LEFT JOIN skills_buff sb2\
+              ON s.buff_id_2 = sb2.skill_idx\
+          LEFT JOIN skills_buff sb3\
+              ON s.buff_id_3 = sb3.skill_idx\
+          LEFT JOIN skills_buff sb4\
+              ON s.buff_id_4 = sb4.skill_idx\
+          LEFT JOIN skills_buff sb5\
+              ON s.buff_id_5 = sb5.skill_idx\
+        WHERE\
+          jl.job_id = ANY($1)\
+          AND jl.race = $2\
+        ORDER BY\
+            jl.level,\
+            jl.race,\
+            s.skill_idx\
+      `,
+      [jobIds, charDetail.race]
+    );
+
+  res.status(200).json({query: skills.rows})
+});
+
+>>>>>>> Stashed changes
 // app.get('/places', async (req, res) => {
 //   const fileContent = await fs.readFile('./data/places.json');
 

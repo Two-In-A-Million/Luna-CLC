acb4d707efa4a685e20c95f58311322a15a14614
Hizkia Halim <hizkiahalim2412001@gmail.com>
2026-02-06 10:51:16 +0700

    update generate skill list using data from backend

--- frontend/src/components/skill-options/SkillOptions.tsx
+++ frontend/src/components/skill-options/SkillOptions.tsx
@@ -3,31 +3,6 @@ import SkillCard from "./SkillCard";
 
 export default function SkillOptions({ skillOptions }: { skillOptions: any[] }) {
 
-//     useEffect(() => {
-//     // get data
-//     async function getJobListData() {
-//       let charDetail = {
-//         race: currRace,
-//         class: currCharClass,
-//         jobName20: 'Fighter',
-//         jobName40: 'None'
-//       }
-
-//       let getJobList = await fetch("http://localhost:3000/get-job-list", {
-//         method: "POST",
-//         body: JSON.stringify({charDetail : charDetail}),
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//       });
-//       let jobList = await getJobList.json();
-//       setJobLists(jobList.query);
-//     }
-
-//     getJobListData();
-
-//   }, [currRace, currCharClass]);
-
     return (
         <div className={classes.skillMainDiv}>
             <div className={classes.skillSelectionTitle}>Skills</div>

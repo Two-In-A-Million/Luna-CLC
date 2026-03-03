2f2bf0d708500385adea8cdecc0d7e9b63d3b0e2
Hizkia Halim <hizkiahalim2412001@gmail.com>
2026-03-03 15:04:29 +0700

    build widget from before vercel
    build the widget and push to public

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build:{
-    lib: {
-      entry: "src/mount.js",
-      name: "MyReactWidget",
-      fileName: () => "my-react-widget.js",
-      formats: ["iife"]
-    }
-  }
-})
+export default defineConfig(({ mode }) => {
+  const isWidget = mode === "widget";
+
+  return {
+    plugins: [react()],
+    build: isWidget
+      ? {
+          lib: {
+            entry: "src/mount.js",
+            name: "LunaSkillCalculator",
+            fileName: () => "luna-skill-calc.js",
+            formats: ["iife"],
+          }
+        }
+      : {},
+  };
+});

8abddd610f1ca588505300401e6a1709a85ec937
jul <julianto.sanjaya@yahoo.com>
2026-03-12 21:33:44 +0700

    Logo, Fix description (tool tip)

--- backend/app.js
+++ backend/app.js
@@ -11,20 +11,6 @@ const app = express();
 
 app.use(express.json());
 
-// CORS
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT");
-//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-//   next();
-// });
-
-// app.use(cors({
-//   origin: "http://localhost:5173",
-//   methods: ["GET", "POST", "PUT", "DELETE"],
-//   allowedHeaders: ["Content-Type", "Authorization"],
-// }));
-
 const allowedOrigins = [
   process.env.STAGING_URL,
   process.env.PROD_URL,
@@ -32,10 +18,17 @@ const allowedOrigins = [
 ].filter(Boolean);
 
 app.use(cors({
-  origin: "http://localhost:5173", // your frontend port
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }))
+
 
 app.set("trust proxy", 1);
 
